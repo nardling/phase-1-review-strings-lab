cb8ec4f9ed5a3da5be2bde840462915cebd5b227
David <david.v.bernardo@gmail.com>
2022-09-20 08:24:50 -0500

    commit tree 8b5443f0af40696bb0b8228e78115781a44a7a20

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+// Write your code in this 
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + ' !';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, ' + currentUser.substring(0, 1) + '!';
